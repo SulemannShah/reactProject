c83d191f77feb7a857b77f78311bef0c81160913
SulemannShah <shahsuleman0077@gmail.com>
2026-03-31 02:25:54 +0500

    add

--- 14-cart/starter/src/action.js
+++
@@ -1,7 +0,0 @@
-export const CLEAR_CART = 'CLEARCART'
-export const REMOVE = 'REMOVE'
-export const INCREASE = 'INCREASE'
-export const DECREASE = 'DECREASE'
-export const LOADING ='LOADING'
-export const DISPLAY_ITEM = 'DISPLAY_ITEM'
-
